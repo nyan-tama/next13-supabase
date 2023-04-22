4589c1305f3258f22093f4d6fafa2829cee6a196
nyan-tama <shake.flake.love@gmail.com>
2023-04-22 20:32:52 +0900

    add ストリーミングhtml

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 import NotesList from "./components/notes-list"
 // cc -> sc はok
 import TimerCounter from "./components/timer-counter"
+// ストリーミングHtmlのために準備
+import { Suspense } from "react"
+import Spinner from "./components/spinner" //loadingのとはまた別
 
 // appディレクトリの中はすべてサーバーコンポーネントとなる
 export default function Page() {
     return (
         <main>
             <div className="m-10 text-center">Hello World😺</div>
-            {/* @ts-ignore */}
-            <NotesList />
+            {/* ストリーミングHtml 個別にローディングも入れられる */}
+            <Suspense fallback={<Spinner color="border-green-500"/>}>
+                {/* @ts-ignore */}
+                <NotesList />
+            </Suspense>
             <TimerCounter />
         </main>
     )
